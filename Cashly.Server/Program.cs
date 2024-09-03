0d826e6c5dc50805b2066b186fc921bb01a65e44
eddyiiz <eddyxsayf@gmail.com>
2024-09-03 10:47:52 +0200

    Auth implementation

--- Cashly.Server/Program.cs
+++ Cashly.Server/Program.cs
@@ -1,6 +1,7 @@
 global using Cashly.Server.Data;
 global using Cashly.Server.Models;
 global using Microsoft.EntityFrameworkCore;
+using Cashly.Server.Services.AuthService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
