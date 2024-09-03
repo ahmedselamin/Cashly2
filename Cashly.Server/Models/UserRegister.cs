c2183df10a4ebc75e6459958375825a1f10f5e93
eddyiiz <eddyxsayf@gmail.com>
2024-09-03 10:38:52 +0200

    UserRegister model

---
+++ Cashly.Server/Models/UserRegister.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cashly.Server.Models;
+
+public class UserRegister
+{
+    [Required]
+    public string Username { get; set; } = string.Empty;
+    [Required, StringLength(10, MinimumLength = 5)]
+    public string Password { get; set; } = string.Empty;
+
+}
