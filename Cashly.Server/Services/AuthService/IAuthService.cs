7d8a31572c9acace43d622577108ce88ea7c190a
eddyiiz <eddyxsayf@gmail.com>
2024-09-03 10:47:43 +0200

    auth interface

---
+++ Cashly.Server/Services/AuthService/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace Cashly.Server.Services.AuthService;
+
+public interface IAuthService
+{
+    Task<ServiceResponse<int>> Register(User user, string password);
+    Task<ServiceResponse<string>> Login(string username, string password);
+}
