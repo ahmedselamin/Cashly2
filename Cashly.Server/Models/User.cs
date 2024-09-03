4f4fa45fb945c05c01db87e31597120b9591541d
eddyiiz <eddyxsayf@gmail.com>
2024-09-03 10:32:07 +0200

    user model

---
+++ Cashly.Server/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Cashly.Server.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public byte[] PasswordHash { get; set; } = new byte[32];
+    public byte[] PasswordSalt { get; set; } = new byte[32];
+}
