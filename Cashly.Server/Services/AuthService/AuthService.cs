be3eb8951828b9fc343ba1b6444b49ac6f3594ef
eddyiiz <eddyxsayf@gmail.com>
2024-09-04 16:07:54 +0200

    creating a token

--- Cashly.Server/Services/AuthService/AuthService.cs
+++ Cashly.Server/Services/AuthService/AuthService.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace Cashly.Server.Services.AuthService;
@@ -6,10 +8,12 @@ namespace Cashly.Server.Services.AuthService;
 public class AuthService : IAuthService
 {
     private readonly DataContext _context;
+    private readonly IConfiguration _config;
 
-    public AuthService(DataContext context)
+    public AuthService(DataContext context, IConfiguration config)
     {
         _context = context;
+        _config = config;
     }
     public Task<ServiceResponse<int>> Register(User user, string password)
     {
@@ -54,5 +58,31 @@ public class AuthService : IAuthService
                 hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             return computedHash.SequenceEqual(passwordHash);
         }
+    }
+
+    private string CreateToken(User user)
+    {
+        List<Claim> claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken
+            (
+              claims: claims,
+              expires: DateTime.Now.AddDays(1),
+              signingCredentials: creds
+            );
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
     }
 }
