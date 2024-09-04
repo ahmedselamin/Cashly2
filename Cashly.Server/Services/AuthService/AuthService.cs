6aa06a65348c050f6e40120e53fce088cad1cd29
eddyiiz <eddyxsayf@gmail.com>
2024-09-04 16:04:40 +0200

    Verify password hash

--- Cashly.Server/Services/AuthService/AuthService.cs
+++ Cashly.Server/Services/AuthService/AuthService.cs
@@ -46,5 +46,13 @@ public class AuthService : IAuthService
         }
     }
 
-
+    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512(passwordSalt))
+        {
+            var computedHash =
+                hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
+        }
+    }
 }
