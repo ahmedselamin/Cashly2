ff8b0630df72d3bad4f180714d1f5c9fedb6b6e4
eddyiiz <eddyxsayf@gmail.com>
2024-09-05 09:51:16 +0200

    fixing auth

--- Cashly.Server/Controllers/authController.cs
+++ Cashly.Server/Controllers/authController.cs
@@ -18,14 +18,19 @@ namespace Cashly.Server.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegister request)
         {
-            var response = await _authService.Register(
-                new User
-                {
-                    Username = request.Username
-                },
-                request.Password);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-            if (response.Success)
+            var response = await _authService.Register(
+             new User
+             {
+                 Username = request.Username
+             },
+             request.Password);
+
+            if (!response.Success)
             {
                 return BadRequest(response.Message);
             }
